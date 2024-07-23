f6691a2343e6230ab9ad233f89fbfb00b3b1c735
salimkhandev <salimt450s@gmail.com>
2024-07-24 00:45:05 +0500

    testing

--- index.js
+++ index.js
@@ -48,9 +48,10 @@ app.post('/submit-form', async (req, res) => {
     else {
       console.log("❌");
             const newData=new MyModel({name:name,email:email,message:message}).save()
+
   .then(() => console.log('Data saved successfully!'))
   .catch(error => console.error('Error saving data:', error));
-      res.status(400).json({ message: 'Captcha verification failed ❌' });
+      res.status(400).json({ message: 'Captcha please verification failed ❌' });
     }
   } catch (error) {
     res.status(500).json({ message: 'Internal server error' });
