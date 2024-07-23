c150dd0b9e0d82df8461da7417e148c5bb161f40
salimkhandev <salimt450s@gmail.com>
2024-07-24 00:07:43 +0500

    testing

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.listen(3000, () => {
+app.listen(Process.env.PORT, (req, res) => {
    console.log(`Server is running on http://localhost:3000`);
 });
