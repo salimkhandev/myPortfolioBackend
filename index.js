fe48bf3d9b95b470568b7e01a1c305a01dafa39e
salimkhandev <salimt450s@gmail.com>
2024-07-24 00:54:14 +0500

    adde

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const port = process.env.PORT ||3000;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json())
 
@@ -18,7 +19,7 @@ app.use(express.json())
 app.get('/',(req,res)=>{
 
 
-  res.send("Hello Salim")
+  res.send("Hello Salim ❤️")
 })
 
 app.post('/submit-form', async (req, res) => {
